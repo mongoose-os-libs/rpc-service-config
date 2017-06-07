f82ed3b57c4b591571d479e2c2353db6f502f6fc
Dmitry Frank <mail@dmitryfrank.com>
2017-06-07 06:05:58 +0100

    Factor out rpc config service
    PUBLISHED_FROM=35126854d4281d1a90ca2b872933ed8b834ad966

---
+++ README.md
@@ -0,0 +1,3 @@
+# RPC Config Service support
+
+Needed for `mos config-get` and `mos config-set` to work.
---
+++ src/mgos_service_config.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2014-2016 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_FW_SRC_MGOS_SERVICE_CONFIG_H_
+#define CS_FW_SRC_MGOS_SERVICE_CONFIG_H_
+
+#if MGOS_ENABLE_RPC
+
+#include <stdbool.h>
+
+#include "fw/src/mgos_init.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/*
+ * Initialises mg_rpc handlers for Config commands
+ */
+bool mgos_rpc_service_config_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* MGOS_ENABLE_RPC */
+#endif /* CS_FW_SRC_MGOS_SERVICE_CONFIG_H_ */
